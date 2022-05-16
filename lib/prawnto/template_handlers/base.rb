9428688357b55663bf1520ce58c41f0aa9faa3b8
Karen Carter <kacarter@users.noreply.github.com>
2022-05-16 09:43:02 -0600

    Update base.rb

--- lib/prawnto/template_handlers/base.rb
+++ lib/prawnto/template_handlers/base.rb
@@ -5,10 +5,10 @@ module Prawnto
     class Base < (base_class_for_template_handler_required? ? ::ActionView::TemplateHandler : ::Object)
       include ::ActionView::TemplateHandlers::Compilable if template_should_include_compilable?
 
-      def self.call(template, source = nil)
+      def self.call(template, source)
         "_prawnto_compile_setup;" +
         "pdf = Prawn::Document.new(@prawnto_options[:prawn]);" +
-        "#{source ? source : template.source}\n" +
+        "#{source}\n" +
         "pdf.render;"
       end
 
